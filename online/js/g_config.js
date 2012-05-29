0c262ccbf76f86f8b5e70c855def591b8da092c6
hongru <hongru.chenhr@gmail.com>
2012-05-29 19:27:26 +0800

    initialize

---
+++ online/js/g_config.js
@@ -0,0 +1,5 @@
+/* global default configs */
+woh.g_config = {
+    canvasWidth: 960,
+    canvasHeight: 480
+};
---
+++ online/js/stage.js
@@ -0,0 +1,11 @@
+/**
+ * stage controler
+ */
+Laro.NS('woh', function (L) {
+    var pkg = this;
+
+    this.init = function () {
+    
+    }
+
+});
