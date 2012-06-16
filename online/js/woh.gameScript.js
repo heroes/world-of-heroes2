fc6789d65febdd92fcf34d90e02291be2d5922f8
Akikonata <66875692@qq.com>
2012-06-16 23:33:00 +0800

    added runtime namespace
    added

--- online/js/woh.gameScript.js
+++ online/js/woh.gameScript.js
@@ -5,18 +5,17 @@
 Laro.NS('woh.gameScript', function (L) {
 
     var script = null;
-    this.stageTag = '1';
+    //this.stageTag = '1';
         
     this.startExec = function(_script){
         script = _script;
-        this.stageTag = '1';
-        with(script[this.stageTag])
+        woh.runtime.stageTag = '1';
+        with(script[woh.runtime.stageTag])
             woh.stage.go(type,data);
     }
     this.continueExec = function(){
-        with(script[this.stageTag =script[this.stageTag].next])
+        with(script[woh.runtime.stageTag =script[woh.runtime.stageTag].next])
             woh.stage.go(type,data);
-            console.log(this.stageTag);
     }
     
 });
--- online/js/woh.runtime.js
+++ online/js/woh.runtime.js
@@ -1,4 +1,4 @@
 //用来存储运行时数据的文件
-woh.runtime_flow={
-	current:[],
-}
+Laro.NS('woh.runtime', function (L) {
+    this.stageTag = '1';//初始化一个用以记录当前剧情进程的值
+});
