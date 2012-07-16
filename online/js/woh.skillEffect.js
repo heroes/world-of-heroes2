37a519bb0f61036a53c0cb3ffa604b07d2a6f634
Akikonata <66875692@qq.com>
2012-07-16 23:36:01 +0800

    铲掉了乱七八糟的注释
    111

--- online/js/woh.skillEffect.js
+++ online/js/woh.skillEffect.js
@@ -23,20 +23,17 @@
             return ret;
         },
         initCheckArea: function () {
-            // ¿É²Ù×÷ÇøÓò == > ÕâÒ»²¿·ÖÊý¾ÝÐèÒª Ìáµ½ ÈËÎïÊý¾ÝÅäÖÃ ÀïÃæÈ¥£¬ ÕâÀïÄ¿Ç°ÔÝÊ±ÏÈÐ´ËÀ
             var me = this;
             this.checkRect = new L.Sprite(woh.stage.$, function () {
                 this.width = 100;
                 this.height = 120;
 
                 this.setPos = function (x, y) {
-                    // ÒòÎª sprite Ä¬ÈÏÊÇ»­ÔÚÖÐÐÄµÄ£¬ËùÒÔ Ò²ÐèÒª¼ÓÉÏÆ«ÒÆÁ¿
                     this.x = x - 50;
                     this.y = y - 60;
                 };
 
                 this.setPos(me.x, me.y);
-                // dev Ä£Ê½£¬¿ÉÒÔÏÔÊ¾¿ÉÒÔ²Ù×÷µÄÇøÓò£¬ÕýÊ½µÄÊ±ºò°ÑdrawÈ¥µô
                 this.draw = function (rd) {
                     rd.drawRect(0, 0, this.width, this.height, '#000');
                 }
@@ -50,13 +47,10 @@
                 ((me.face == 'left' && !o.renderMirrored) || (me.face == 'right' && o.renderMirrored)) && o.mirror();
                 o.draw(render, x, y, 0, 1, null);
             });
-            this.drawHPBar(render);
-            //console.log(ctx);
-
         },
 
         normalAttack: function () {
-            this.fsm.setState(woh.roleStates.attack);
+            
         }
     });
     this.SkillEffect = SkillEffect;
