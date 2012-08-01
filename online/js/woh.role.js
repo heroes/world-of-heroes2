6384a6bfb909ccf30cdac971c9458ce9ce4ee117
Akikonata <66875692@qq.com>
2012-08-01 15:17:34 +0800

    开始游戏时清空已执行队列
    11

--- online/js/woh.role.js
+++ online/js/woh.role.js
@@ -70,7 +70,8 @@
             this.curAnimation && this.curAnimation.forEach(function (o) {
                 ((me.face == 'left' && !o.renderMirrored) || (me.face == 'right' && o.renderMirrored)) && o.mirror();
                 o.draw(render, x, y, 0, 1, null);
-            });             /***debug***/
+            });             
+            /***debug***/
             render.context.fillStyle = "blue";
             render.context.fillRect(this.x - 2, this.y - 2, 4, 4);
             render.context.strokeStyle = "green";
