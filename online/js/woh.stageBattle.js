a3946c0ebee0a732a81bef9c1c56537bd9cde568
Akikonata <66875692@qq.com>
2012-08-11 13:20:44 +0800

    加上了try和catch
    11

--- online/js/woh.stageBattle.js
+++ online/js/woh.stageBattle.js
@@ -20,7 +20,12 @@ Laro.NS('woh.stageClass', function (L) {
             setTimeout(function () {woh.util.fadeIn(woh.els.canvasWrap)}, 500);
             
             //woh.util.playOneSound('music/battle-boss.mp3');//: woh.util.playOneSound('music/battle-normal.mp3');
-            woh.util.playOneSound(data.bgm);
+            try{
+                woh.util.playOneSound(data.bgm);
+            }
+            catch(expection){
+                
+            }
             this.timeInState = 0;
             this.gameOverTime = 0;
             // temp part to do area
