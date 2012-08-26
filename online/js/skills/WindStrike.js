eae3f00d5234ae46e14003a3a0c10b90e9151c20
winter <csf178@gmail.com>
2012-08-26 22:21:56 +0800

    clear master

--- online/js/skills/WindStrike.js
+++
@@ -1,9 +0,0 @@
-Laro.NS('woh', function (L) {
-    this.WindStrike = L.Class(function (data,level) {
-        this.damage = data.damage[level] ;
-    }).methods({
-        convert:function(d){
-            d.damage+= this.damage;
-        }
-    });
-})
